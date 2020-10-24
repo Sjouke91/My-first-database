a6b3e1bd3ac70971c02c765427aee66d369d2304
Sjouke91 <s.bosma1991@gmail.com>
2020-10-24 11:31:36 +0200

    Setting up database

---
+++ seeders/20201023210723-some-todoLists.js
@@ -0,0 +1,26 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert(
+      "todoLists",
+      [
+        {
+          name: "making assessment",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: "washing dishes",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete("todoLists", null, {});
+  },
+};
---
+++ seeders/20201023211404-some-todoItems.js
@@ -0,0 +1,28 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert(
+      "todoItems",
+      [
+        {
+          task: "making assessment",
+          deadline: "Tomorrow",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          task: "making diner",
+          deadline: "yesterday",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete("todoItems", null, {});
+  },
+};
